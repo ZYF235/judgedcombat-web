4c66a4424b3cc59b4dc432c5788d6f5bdb875ffa
留行亭长丶 <zhuyifa1028@gmail.com>
2022-08-18 15:22:16 +0800

    1

---
+++ src/components/DictSelect/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-select v-model="clueInfo.involveDomain" placeholder="请选择涉及领域" @change="fetchInvolveAccusation">
+    <el-option v-for="item in options.involveDomain" :key="item.dicId" :value="item.dicId" :label="item.name"/>
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: 'DictSelect'
+}
+</script>
+
+<style scoped>
+
+</style>
